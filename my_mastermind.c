10970af8b6380e670b857087a69e24e88b89ba7c
Shino <100228440+LargyThomas@users.noreply.github.com>
2026-02-20 12:22:21 +0100

    Update my_mastermind.c

--- my_mastermind.c
+++ my_mastermind.c
@@ -3,142 +3,126 @@
 #include <time.h>
 #include <unistd.h>  // read()
 
-void generation_code(char *code) {
-    int utiliser[9] = {0};
+// Generate a 4-digit code if the user doesn't provide one.
+// Each digit is unique and between 0 and 8.
+void generate_code(char *code) {
+    int used[9] = {0};
     int i = 0;
     while (i < 4) {
-        int chiffre = rand() % 9;
-        if (!utiliser[chiffre]) {
-            utiliser[chiffre] = 1;
-            code[i] = '0' + chiffre;
+        int digit = rand() % 9;
+        if (!used[digit]) {
+            used[digit] = 1;
+            code[i] = '0' + digit;
             i++;
         }
     }
     code[4] = '\0';
 }
 
-int strlen_bis(char* chaine) {
+// Recreation of the existing function str_len.
+// Returns the length of a string without counting the null terminator.
+int strlen_bis(char* str) {
     int len = 0;
-    while (chaine[len] != '\0')
+    while (str[len] != '\0')
         len++;
     return len;
 }
 
+// Validates the format of the code: must be 4 unique digits between 0 and 8.
 int validation_format(char *code) {
     if (!code || strlen_bis(code) != 4)
         return 0;
-    int vu[9] = {0};
+    int seen[9] = {0};
     for (int i = 0; i < 4; i++) {
         if (code[i] < '0' || code[i] > '8')
             return 0;
-        int chiffre = code[i] - '0';
-        if (vu[chiffre])
+        int digit = code[i] - '0';
+        if (seen[digit])
             return 0;
-        vu[chiffre] = 1;
+        seen[digit] = 1;
     }
     return 1;
 }
 
-void compare_codes(char *code_secret, char *code_user, int *bien_places, int *mall_places) {
+// Compares the secret code with the user's guess.
+// Counts well-placed and misplaced digits.
+void compare_codes(char *secret, char *guess, int *well_placed, int *misplaced) {
     int i;
-    int compteur_secret[9] = {0};
-    int compteur_user[9] = {0};
-
-    *bien_places = 0;
-    *mall_places = 0;
-
-
+    int count_secret[9] = {0};
+    int count_guess[9] = {0};
+    *well_placed = 0;
+    *misplaced = 0;
     for (i = 0; i < 4; i++) {
-        if (code_secret[i] == code_user[i])
-            (*bien_places)++;
+        if (secret[i] == guess[i])
+            (*well_placed)++;
         else {
-            
-            compteur_secret[code_secret[i] - '0']++;
-            compteur_user[code_user[i] - '0']++;
+            count_secret[secret[i] - '0']++;
+            count_guess[guess[i] - '0']++;
         }
     }
-
     for (i = 0; i < 9; i++) {
-        if (compteur_secret[i] > 0 && compteur_user[i] > 0) {
-            if (compteur_secret[i] < compteur_user[i]) {
-                *mall_places += compteur_secret[i];
-            }
-            else {
-                *mall_places += compteur_user[i];
-            }
+        if (count_secret[i] > 0 && count_guess[i] > 0) {
+            if (count_secret[i] < count_guess[i])
+                *misplaced += count_secret[i];
+            else
+                *misplaced += count_guess[i];
         }
     }
 }
 
-int lecture_code(char* buffer, int maxlen) {
-    size_t lecture;
+// Reads a line from stdin character by character.
+// Returns 1 if EOF is reached with no input, 0 otherwise.
+int read_code(char* buffer, int maxlen) {
+    size_t bytes_read;
     char c;
     int len = 0;
-    while((lecture = read(0, &c, 1)) > 0) {
+    while ((bytes_read = read(0, &c, 1)) > 0) {
         if (c == '\n') break;
-        if (len < maxlen - 1) {
+        if (len < maxlen - 1)
             buffer[len++] = c;
-        }
     }
-    if (lecture == 0 && len == 0)
+    if (bytes_read == 0 && len == 0)
         return 1;
     buffer[len] = '\0';
     return 0;
 }
 
-void gestion_arguments(int argc, char* argv[], char* code_secret, int* attempts) {
+// Parses command-line arguments to set the secret code and number of attempts.
+// -c <code>     : sets a custom secret code (must be valid)
+// -t <attempts> : sets the number of allowed attempts (default: 10)
+void handle_arguments(int argc, char* argv[], char* secret, int* attempts) {
     *attempts = 10;
     int has_secret = 0;
-
     for (int i = 1; i < argc; i++) {
         if (argv[i][0] == '-' && argv[i][1] == 'c' && i + 1 < argc) {
-            if (validation_format(argv[i+1])) {
+            if (validation_format(argv[i + 1])) {
                 for (int j = 0; j < 5; j++)
-                    code_secret[j] = argv[i+1][j];
+                    secret[j] = argv[i + 1][j];
                 has_secret = 1;
             }
         }
         if (argv[i][0] == '-' && argv[i][1] == 't' && i + 1 < argc)
-            *attempts = atoi(argv[i+1]);
+            *attempts = atoi(argv[i + 1]);
     }
-
     if (!has_secret)
-        generation_code(code_secret);
+        generate_code(secret);
 }
 
 int main(int argc, char** argv) {
     srand(time(NULL));
     int attempts;
-    char code_secret[5];
+    char secret[5];
 
-    gestion_arguments(argc, argv, code_secret, &attempts);
+    handle_arguments(argc, argv, secret, &attempts);
 
     printf("Will you find the secret code?\n");
     printf("Please enter a valid guess\n");
 
     for (int round = 0; round < attempts; round++) {
         printf("---\nRound %d\n>", round);
-
-        char code_user[5];
-        if (lecture_code(code_user, 5) == 1)
+        char guess[5];
+        if (read_code(guess, 5) == 1)
             return 0;
-
-        if (!validation_format(code_user)) {
+        if (!validation_format(guess)) {
             printf("Wrong input!\n");
             round--;
-            continue;
-        }
-
-        int bien, mal;
-        compare_codes(code_secret, code_user, &bien, &mal);
-
-        if (bien == 4) {
-            printf("Congratz! You did it!\n");
-            return 0;
-        }
-
-        printf("Well placed pieces: %d\n", bien);
-        printf("Misplaced pieces: %d\n", mal);
-    }
-    return 0;
-}
